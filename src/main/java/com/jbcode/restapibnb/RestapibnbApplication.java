69a06a5c00842f6ea6f100687a27f757f9b75589
jbcode <78531307+jbcodefr@users.noreply.github.com>
2021-03-19 23:19:20 +0100

    initial commit base spring

---
+++ src/main/java/com/jbcode/restapibnb/RestapibnbApplication.java
@@ -0,0 +1,13 @@
+package com.jbcode.restapibnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapibnbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapibnbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jbcode/restapibnb/RestapibnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbcode.restapibnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapibnbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
