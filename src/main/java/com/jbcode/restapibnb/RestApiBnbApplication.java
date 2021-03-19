81370496100e0595069d9805fb2906a463171600
jbcode <78531307+jbcodefr@users.noreply.github.com>
2021-03-19 23:27:46 +0100

    config commit of the project

--- src/main/java/com/jbcode/restapibnb/RestApiBnbApplication.java
+++ src/main/java/com/jbcode/restapibnb/RestApiBnbApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestapibnbApplication {
+public class RestApiBnbApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestapibnbApplication.class, args);
+        SpringApplication.run(RestApiBnbApplication.class, args);
     }
 
 }
